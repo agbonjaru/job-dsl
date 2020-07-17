680df9168377e75640e870dbe5546b6776f716e7
agbonjaru <agbonjaru>
2020-07-17 07:06:50 -0700

    docker groovy

---
+++ nodejsdocker.groovy
@@ -0,0 +1,35 @@
+job('notch-angular-docker'){
+    scm {
+        git('git://github.com/agbonjaru/notch.git'){
+            node -> //is hudson.plugin.git.GitSCM
+            node / gitConfigName('agbonjaru')
+            node / gitConfigEmail('jstryngs@gmail.com')
+        
+        }
+    }
+triggers {
+    scm('H/5 * * * *')  //runs every 5 mins
+}
+wrappers {
+    nodejs('nodejs') //this is the name of node js installation
+                     //Manage jenkins -> Configure Tools -> NodeJs installation
+
+}
+steps {
+    
+    // shell("npm install")
+    dockerBuildAndPublish {
+        repositoryName('jstryngs/notch-angular-docker')
+        tag('${GIT_REVISION,length=9}')
+        registryCredentials('dockerHub')
+        forcePull(false)
+        forceTag(false)
+        createFingerprints(false)
+        skipDecorate()
+
+    }
+
+
+}
+
+}
