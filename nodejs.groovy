9a1e91f9d7f9fb7f38c34fe7853ad8c58e0fd4cb
agbonjaru <agbonjaru>
2020-07-17 00:51:31 -0700

    groovy config

---
+++ nodejs.groovy
@@ -0,0 +1,22 @@
+job('NodeJS example'){
+    scm {
+        git('git://github.com/agbonjaru/notch.git'){
+            node -> //is hudson.plugin.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jstryngs@gmail.com')
+        
+        }
+    }
+triggers {
+    scm('H/5 * * * *')
+}
+wrappers {
+    nodejs('nodejs') //this is the name of node js installation
+                     //Manage jenkins -> Configure Tools -> NodeJs installation
+
+}
+steps {
+    shell("npm install")
+}
+
+}
